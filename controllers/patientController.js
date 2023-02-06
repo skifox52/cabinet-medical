3b09a5ead0d1eb22bf9a7a2a2d2baaa613767927
skifox52 <fet.a@hotmail.fr>
2023-02-06 23:00:28 +0100

    working on register form validation

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -3,7 +3,9 @@ const PatientModel = require("../models/PatientModel")
 //Post a patient
 exports.postPatient = async (req, res) => {
   try {
-    console.log(req.body.nom)
+    const { nom, prenom, adresse, dn, sexe, tel, gs } = req.body
+    const newUser = new PatientModel(nom, prenom, adresse, dn, sexe, tel, gs)
+    await newUser.save()
     res.status(201).render("index")
   } catch (err) {
     res.status(400)
